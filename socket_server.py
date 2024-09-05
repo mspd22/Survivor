262c7e118fdb281768c0c65acf7f59c100ee386d
M S Pramod <161943256+mspd22@users.noreply.github.com>
2024-09-05 16:26:10 +0530

    Update socket_server.py

--- socket_server.py
+++ socket_server.py
@@ -1,13 +1,15 @@
 import socket
 from pygame import mixer
 
+port = 50550
+
 def start_socket_server():
     # Initialize Pygame's sound mixer
     mixer.init()
 
     # Create a socket server
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('127.0.0.1', 50550))  # Bind to localhost on port 50550
+    server_socket.bind(('127.0.0.1', port))  # Bind to localhost on port 50550
     server_socket.listen(1)
     
     print("\nSocket server is listening on port 50550...")
